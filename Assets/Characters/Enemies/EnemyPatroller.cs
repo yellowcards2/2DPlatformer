233ee746b716a83373cea0c70734652e4844a4eb
yellowcards2 <151522822+yellowcards2@users.noreply.github.com>
2025-09-12 05:36:50 +0300

    2DPlatformer

--- Assets/Characters/Enemies/EnemyPatroller.cs
+++ Assets/Characters/Enemies/EnemyPatroller.cs
@@ -6,12 +6,13 @@ public class EnemyPatroller : MonoBehaviour
     [SerializeField] private Transform[] _waypoints;
 
     private int _index;
+    private float _distanceValue = 0.2f;
 
     private void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, _waypoints[_index].position, _speed * Time.deltaTime);
 
-        if (Vector2.Distance(transform.position ,_waypoints[_index].position) < 0.2f)
+        if (Vector2.Distance(transform.position ,_waypoints[_index].position) < _distanceValue)
         {
             if (_index > 0)
             {                
